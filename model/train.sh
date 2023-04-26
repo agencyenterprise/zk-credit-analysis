8a91be0aeb3d11752199bfe5440c6a97b508915e
Lorenzo Alencar Tomaz <lorenzotomaz@MacBook-Pro-de-Lorenzo.local>
2023-04-26 19:12:02 -0300

    added unformated and ugly model code

---
+++ model/train.sh
@@ -0,0 +1,6 @@
+docker build -t fhe_score_model -f Dockerfile . && \
+docker run -it fhe_score_model --name fhe_score_model && \
+docker cp fhe_score_model:/project/dev . && \
+docker cp fhe_score_model:/project/hf_cache . && \
+
+docker rm "$(docker ps -a --filter name=fhe_score_model -q)"
